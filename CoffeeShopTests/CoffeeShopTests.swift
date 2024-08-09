a67b733f8209f32cb6ad3ddd41e43e1b5b69ac73
Marco Abundo <shrtlist@gmail.com>
2024-08-08 20:15:41 -0700

    Unit tests

--- CoffeeShopTests/CoffeeShopTests.swift
+++ CoffeeShopTests/CoffeeShopTests.swift
@@ -10,27 +10,20 @@ import XCTest
 
 final class CoffeeShopTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testNumberFormatter() {
+        let testValue = 4.700001
+        let formattedRating = NumberFormatter.oneDecimalPlace.string(from: NSNumber(value: testValue))!
+        let numberOfCharacters = formattedRating.count
+
+        XCTAssertEqual(numberOfCharacters, 3, "Number of characters must be 3")
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    @MainActor func testfetchCoffeeShops() async {
+        let sut = CoffeeShopViewModel()
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+        await sut.fetchCoffeeShops()
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssertFalse(sut.isLoading)
+        XCTAssertTrue(sut.coffeeShops.count > 0)
     }
-
 }
