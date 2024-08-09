cb07679687df042f0a3c5c1f10c00247e9d73358
Marco Abundo <shrtlist@gmail.com>
2024-08-08 18:40:25 -0700

    New file

--- CoffeeShop/Views/CoffeeShopListView.swift
+++ CoffeeShop/Views/CoffeeShopListView.swift
@@ -36,36 +36,6 @@ struct CoffeeShopListView: View {
     }
 }
 
-struct CoffeeShopRow: View {
-    let business: YelpBusiness
-
-    var body: some View {
-        HStack {
-            AsyncImage(url: URL(string: business.imageUrl)) { image in
-                image
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 80, height: 80)
-                    .cornerRadius(8)
-            } placeholder: {
-                ProgressView()
-                    .frame(width: 80, height: 80)
-            }
-
-            VStack(alignment: .leading) {
-                Text(business.name)
-                    .font(.headline)
-                Text("\(business.location.address1), \(business.location.city), \(business.location.state)")
-                    .font(.subheadline)
-                Text("Price: \(business.price ?? "N/A")")
-                    .font(.subheadline)
-                Text("Rating: \(business.rating)")
-                    .font(.subheadline)
-            }
-        }
-    }
-}
-
 #Preview {
     CoffeeShopListView()
 }
