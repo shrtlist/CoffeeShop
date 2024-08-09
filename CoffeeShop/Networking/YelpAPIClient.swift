49d02f681e6a27dbab3c04f8abbdb097935159af
Marco Abundo <shrtlist@gmail.com>
2024-08-08 19:10:34 -0700

    private let variables for parameters

--- CoffeeShop/Networking/YelpAPIClient.swift
+++ CoffeeShop/Networking/YelpAPIClient.swift
@@ -7,9 +7,14 @@
 
 import Foundation
 import Alamofire
+import CoreLocation
 
 class YelpAPIClient {
     private let apiKey = "GhMnYVSy1cHNqJhgOhqPkVwzPcXzEXScOfUfrYDMEnlR4RmHLdnzdipqAt3esgmyfgiAOjJ8vdrzBfW9-ZBtia3oU6gX-jTg0eZRbBne6hjqg7ASopMdTr3otVW1ZnYx"
+    private let url = "https://api.yelp.com/v3/businesses/search"
+    private let resultLimit = 10
+    private let searchTerm = "coffee shops"
+    private let coordinate = CLLocationCoordinate2D(latitude: 37.776575, longitude: -122.394978)
 
     private let decoder: JSONDecoder = {
         let decoder = JSONDecoder()
@@ -18,12 +23,11 @@ class YelpAPIClient {
     }()
 
     func fetchCoffeeShops(offset: Int = 0) async throws -> [YelpBusiness] {
-        let url = "https://api.yelp.com/v3/businesses/search"
         let parameters: [String: Any] = [
-            "term": "coffee shops",
-            "latitude": 37.776575,
-            "longitude": -122.394978,
-            "limit": 10,
+            "term": searchTerm,
+            "latitude": coordinate.latitude,
+            "longitude": coordinate.longitude,
+            "limit": resultLimit,
             "offset": offset
         ]
         let headers: HTTPHeaders = [
