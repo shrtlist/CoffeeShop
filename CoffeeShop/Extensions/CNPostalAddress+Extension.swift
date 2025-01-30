a6987742f70e1b4228eec07435b4d5d016a27ff7
Marco Abundo <shrtlist@gmail.com>
2025-01-29 21:11:57 -0800

    Exclude country

--- CoffeeShop/Extensions/CNPostalAddress+Extension.swift
+++ CoffeeShop/Extensions/CNPostalAddress+Extension.swift
@@ -9,6 +9,16 @@ import Contacts
 
 extension CNPostalAddress {
     var addressString: String {
-        return CNPostalAddressFormatter.string(from: self, style: .mailingAddress)
+        let mutableAddress = CNMutablePostalAddress()
+        mutableAddress.street = self.street
+        mutableAddress.city = self.city
+        mutableAddress.state = self.state
+        mutableAddress.postalCode = self.postalCode
+        mutableAddress.subLocality = self.subLocality
+        mutableAddress.subAdministrativeArea = self.subAdministrativeArea
+        // Exclude country (don't set `mutableAddress.country`)
+        
+        let formatter = CNPostalAddressFormatter()
+        return formatter.string(from: mutableAddress).trimmingCharacters(in: .whitespacesAndNewlines)
     }
 }
